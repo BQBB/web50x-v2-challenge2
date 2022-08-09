996f86eb02508020f0b22c0b9a9a5c9cc2deddd8
unknown <abutheralatar@gmail.com>
2022-08-09 15:06:13 +0300

    desifnation has completed

--- challenge-2.py
+++ challenge-2.py
@@ -27,16 +27,50 @@ if oct5 == 8 or oct5 == 16 or oct5 == 24 or oct5 == 32:
 else:
     print("error oct5 not in range")
 
-
+# section for checking classes
 if oct1 <= 127:
-    print("calss A")
+    # print("calss A")
+    ip_class = "A"
 elif oct1 >= 128 and oct1 <= 191:
-    print("class B")
+    # print("class B")
+    ip_class = "B"
 elif oct1 >= 192 and oct1 <= 223:
-    print("class C")
+    # print("class C")
+    ip_class = "C"
 elif oct1 >= 224 and oct1 <= 239:
-    print("class D")
+    # print("class D")
+    ip_class = "D"
 elif oct1 >= 240 and oct1 <= 255:
-    print("class E")
-    
+    # print("class E")
+    ip_class = "E"
 
+
+# designation section
+if ip_class == "A":
+    if oct1 == 127 and oct2 == 0 and oct3 == 0 and oct4 == 1:
+        ip_designation = "special"
+    elif oct1 == 10:
+        ip_designation = "private"
+    else:
+        ip_designation = "public"
+if ip_class == "B":
+    if oct1 == 169 and oct2 == 254:
+        ip_designation = "automatic-private"
+    elif oct1 == 172 and (oct2 >= 16 and oct2 <= 31):
+        ip_designation = "private"
+    else:
+        ip_designation = "public"
+if ip_class == "C":
+    if oct1 == 192 and oct2 == 168:
+        ip_designation = "private"
+    else:
+        ip_designation = "public"
+
+if ip_class == "D":
+    ip_designation = "multicasting"
+
+if ip_class == "E":
+    ip_designation = "multicasting"
+
+
+print(f"class:{ip_class}, Designation:{ip_designation} ")
