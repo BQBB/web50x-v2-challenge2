4ef1712921cb1f0a9ef12789ab197c5dd68313fb
unknown <abutheralatar@gmail.com>
2022-08-09 13:02:02 +0300

    section of class has complete

--- challenge-2.py
+++ challenge-2.py
@@ -21,7 +21,22 @@ if oct3 < 0 or oct3 > 255:
     print("error oct3 not in range")
 if oct4 < 0 or oct4 > 255:
     print("error oct4 not in range")
-if oct5 != 8 or oct5 != 16 :#or oct5 != 24 or oct5 != 32:
+# if oct5 != 8  or  oct5 != 16  or  oct5 !=  24  or  oct5 != 32:
+if oct5 == 8 or oct5 == 16 or oct5 == 24 or oct5 == 32:
+    print()
+else:
     print("error oct5 not in range")
 
-print(oct5)
+
+if oct1 <= 127:
+    print("calss A")
+elif oct1 >= 128 and oct1 <= 191:
+    print("class B")
+elif oct1 >= 192 and oct1 <= 223:
+    print("class C")
+elif oct1 >= 224 and oct1 <= 239:
+    print("class D")
+elif oct1 >= 240 and oct1 <= 255:
+    print("class E")
+    
+
